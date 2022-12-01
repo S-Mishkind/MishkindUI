e2c7e1d65094fcf417562fb3163158743cdf7946
S-Mishkind <mishkindstephen@gmail.com>
2022-11-30 17:00:03 -0700

    timer working

---
+++ src/app/Timer/timer/timer.component.ts
@@ -0,0 +1,52 @@
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+
+@Component({
+  selector: 'app-timer',
+  templateUrl: './timer.component.html',
+  styleUrls: ['./timer.component.css']
+})
+export class TimerComponent  {
+
+  display: any;
+  public timerInterval: any;
+
+  constructor() {
+    // this.timer(2);
+  }
+
+  start() {
+    this.timer(2);
+  }
+  stop() {
+    clearInterval(this.timerInterval);
+  }
+
+  timer(minute: number) {
+    // let minute = 1;
+    let seconds: number = minute * 60;
+    let textSec: any = '0';
+    let statSec: number = 60;
+
+    const prefix = minute < 10 ? '0' : '';
+
+    this.timerInterval = setInterval(() => {
+      seconds--;
+      if (statSec != 0) statSec--;
+      else statSec = 59;
+
+      if (statSec < 10) {
+        textSec = '0' + statSec;
+      } else textSec = statSec;
+
+      this.display = `${prefix}${Math.floor(seconds / 60)}:${textSec}`;
+
+      if (seconds == 0) {
+        console.log('finished');
+        clearInterval(this.timerInterval);
+      }
+    }, 1000);
+  }
+
+
+
+}
