6ad59763ca54bea64d06ba1e2c6e53bfb5ac0632
S-Mishkind <mishkindstephen@gmail.com>
2022-11-30 17:30:35 -0700

    making timer progress

--- src/app/Timer/timer/timer.component.ts
+++ src/app/Timer/timer/timer.component.ts
@@ -15,30 +15,39 @@ export class TimerComponent  {
   }
 
   start() {
-    this.timer(2);
+    this.timer(20);
   }
   stop() {
     clearInterval(this.timerInterval);
   }
 
-  timer(minute: number) {
+ // timer(minute: number) {
+    timer(s: number) {
     // let minute = 1;
-    let seconds: number = minute * 60;
+    //let seconds: number = minute * 60;
+    let seconds = s;
     let textSec: any = '0';
-    let statSec: number = 60;
+   // let statSec: number = 60;
+   let statSec = s;
 
-    const prefix = minute < 10 ? '0' : '';
+    //const prefix = minute < 10 ? '0' : '';
 
     this.timerInterval = setInterval(() => {
       seconds--;
-      if (statSec != 0) statSec--;
-      else statSec = 59;
+     if (statSec != 0) statSec--;
+     // else statSec = 59;
 
       if (statSec < 10) {
         textSec = '0' + statSec;
       } else textSec = statSec;
 
-      this.display = `${prefix}${Math.floor(seconds / 60)}:${textSec}`;
+
+
+
+     // this.display = `${prefix}${Math.floor(seconds / 60)}:${textSec}`;
+     // this.display = `${Math.floor(seconds / 60)}:${textSec}`;
+     this.display = `${textSec} seconds`;
+
 
       if (seconds == 0) {
         console.log('finished');
