ac27788e91689d79b6b80f8b6a19b2c1521d15ba
S-Mishkind <mishkindstephen@gmail.com>
2022-12-01 09:55:54 -0700

    12-1 10 am

--- src/app/Services/hiui-service.service.ts
+++ src/app/Services/hiui-service.service.ts
@@ -5,36 +5,23 @@ import { catchError, Observable, tap, throwError } from 'rxjs';
 import { Timer } from '../Models/timer';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class HiuiServiceService {
+  constructor(private http: HttpClient) {}
+  private apiBase = 'https://localhost:7025/api/';
 
-  constructor(private http: HttpClient) { }
-  private apiBase = 'https://localhost:7025/api/'
-
-  inventory$ = this.http.get<Inventory[]>(this.apiBase + 'Atest')
-  .pipe(
-    tap(inventory => console.log('inventory', JSON.stringify(inventory))),
+  inventory$ = this.http.get<Inventory[]>(`${this.apiBase}Atest`).pipe(
+    tap((inventory) => console.log('inventory', JSON.stringify(inventory))),
     catchError(this.handleError)
-  )
+  );
 
-
-
-  timer$ = this.http.get<Timer[]>(this.apiBase + 'Timer?timerLength=15' )
-  .pipe(
-    tap(timer => console.log('timer', JSON.stringify(timer))),
+  timer$ = this.http.get<Timer[]>(`${this.apiBase}Timer?timerLength=15`).pipe(
+    tap((timer) => console.log('timer', JSON.stringify(timer))),
     catchError(this.handleError)
-  )
+  );
 
-/*   getTimer(timerLength: number): Observable<Timer[]>{
-    return this.http.get<Timer[]>(this.apiBase + 'Timer?timerLength='+ timerLength  )
-    .pipe(
-      tap(timer => console.log('timer', JSON.stringify(timer))),
-      catchError(this.handleError)
-    )
-  } */
-
-  private handleError(err: HttpErrorResponse): Observable<never>{
+  private handleError(err: HttpErrorResponse): Observable<never> {
     let errorMessage: string;
     errorMessage = `an error occured - is Local Api Running ? : ${err.status}: ${err.message}`;
     console.error(err);
